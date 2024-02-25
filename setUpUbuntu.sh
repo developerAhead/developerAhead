38dd8153631f65415f7d7d67b4922e587a7d143f
smeet raj <125180048+developerAhead@users.noreply.github.com>
2024-02-25 11:16:26 +0530

    added script to set-up my ubuntu pc

---
+++ setUpUbuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if the kernel name does not contain "Ubuntu"
+if ! uname -a | grep -qi "ubuntu"; then
+    echo "This system is not running Ubuntu. Exiting."
+    exit 1
+fi
+
+sudo apt update 
+sudo apt upgrade
+
+# Download and install Google Chrome if Chrome not installed
+if ! [ -x "$(command -v google-chrome)" ]; then
+    wget -qO- https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
+    sudo apt install ./google-chrome-stable_current_amd64.deb
+    rm -f google-chrome-stable_current_amd64.deb
+else
+    echo "Google Chrome is already installed."
+fi
+
+# Install other Important stuff
+sudo apt install build-essentials samba ark git dolphin kate kwrite konqueror meld cutecom putty vlc default-jre xfce4 xfce4-goodies wine vim ubuntu-restricted-extras wireshark automake cmake net-tools x264 x265 bison flex fakeroot gcc multili g++-multilib expect ncurses-dev ffmpeg libgstreamer1.0.dev libssl-dev manpages-posix manpages-posix-dev kcalc kded5 xdotool openssh-server dolphin-plugins lib32zl
